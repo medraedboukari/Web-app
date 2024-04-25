a47a62048e67a7b8540e264cb106454955a564ab
medraedboukari <153066554+medraedboukari@users.noreply.github.com>
2024-04-25 23:25:13 +0100

    Add files via upload

---
+++ app.jl
@@ -0,0 +1,26 @@
+using GenieFramework
+@genietools
+
+@app begin
+    
+    @in N::Int32 = 1000
+    @in amp::Float32 = 0.25
+    @in freq::Int32 = 1
+    @in ph::Float64 = 3.14/100
+    @in offset::Int32 = 0
+
+    
+    @out my_sine = PlotData()
+
+    @onchange N, amp, freq ,ph,offset begin
+        x = range(0, 1, length=N)
+        y = amp*sin.(2*π*freq*x.+ph).+offset
+        
+        my_sine = PlotData(x=x, 
+                           y=y, 
+                           plot=StipplePlotly.Charts.PLOT_TYPE_LINE)
+    end
+
+end
+    
+@page("/", "app.jl.html")
